581c3cdd01ca05bec3fd59196ea7ddbbe638e251
Regina Szulc-Andrzejewska <renia@j7.pl>
2024-02-07 14:06:04 +0100

    [elementy] poprawka wizualna kafelków zgodnie z ustaleniami

--- resources/views/front/elements/loop/shop.blade.php
+++ resources/views/front/elements/loop/shop.blade.php
@@ -1,18 +1,14 @@
 <div class="col-3 my-3 {{ $element->tags }}">
     <div class="item shop">
         		<a href="{{ $menu->slug }}/{{$element->element_id}}-{{ $element->slug }}" class="animate-box">
-	        		<!--<a href="{{ asset('images/elements/'.$element->image) }}" class="image-popup fh5co-board-img" title="{{ $element->title }}">-->
-                                    @if(!empty($element->image))
-                                    <img src="{{ asset('images/elements/'.$element->image) }}" alt="{{ (!empty($element->title)) ? $element->title : substr($element->description, 0, 100) }}">
-                                    @else
+	        		
                                     <div class="caffle" style="height: 158px;">
-                                  
+                                        @if(!empty($element->image))
+                                        <img src="{{ asset('images/elements/'.$element->image) }}" alt="{{ (!empty($element->title)) ? $element->title : substr($element->description, 0, 100) }}" width="40" height="30">
+                                      @endif
                                         {{ substr(strip_tags($element->description), 0, 200) }}@if(strlen($element->description) > 200) ... @endif</div>
-                                    @endif
-                                    <!--<span class="icon-corner"><i class="fa fa-video" style="color: #fff;"></i></span>-->
-                                    
-                                    
-                                    <!--</a>-->
+                                  
+                                  
         		</a>
         		<div class="fh5co-desc">
                              <div class="row d-flex px-4 mt-1"> 
